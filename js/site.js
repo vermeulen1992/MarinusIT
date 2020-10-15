d7b014f5f6f7d858a1c8f68a288abc3e53df9fd8
Leon Vermeulen <leon@marinusit.nl>
2020-10-15 14:46:22 +0200

    Skills in multiple diagrams

--- js/site.js
+++ js/site.js
@@ -12,31 +12,28 @@
 		$('.brand').localScroll();
 		$('.container').localScroll();
 
+		$("#menu-main").on("click", "a", null, function () {
+			$(".nav-collapse").first().collapse('toggle');
+		});
+
 		document.fonts.ready.then(function () {
-
-			var canvas = document.getElementById('skillsChart');
-			let data = [{
-				backgroundColor: '#c82428',
-				data: [6, 5, 5,4,4,4,4,3,1,1],
-				barPercentage: 0.75
-			}];
-			let labels = ["HTML,CSS,JS","C#","SQL","ASP.Net","Git","SCRUM","Azure DevOps / TFS","PowerShell","Angular","GitLab"]
-
-			let myChart = new Chart(canvas, {
+			let chartConfig = {
 				type: 'horizontalBar',
-				data: {
-					labels: labels,
-					datasets: data
-				},
 				options: {
 					scales: {
 						xAxes: [{
+							gridLines: {
+								display:false
+							},
 							ticks: {
 								beginAtZero: true,
 								display: false
 							}
 						}],
 						yAxes: [{
+							gridLines: {
+								display:false
+							},
 							ticks: {
 								display: false
 							}
@@ -51,6 +48,7 @@
 							ctx.textAlign = "left";
 							ctx.font = "16px Roboto Condensed";
 							ctx.fillStyle = "#fff";
+							var labels = this.data.labels;
 
 							Chart.helpers.each(
 								this.data.datasets.forEach(function(dataset, i) {
@@ -82,6 +80,57 @@
 						enabled: false
 					}
 				}
-			});
+			};
+			var lfChart = document.getElementById('lf-chart');
+			var tfChart = document.getElementById('tf-chart');
+			var mChart = document.getElementById('m-chart');
+
+			let lfData = [{
+				backgroundColor: '#c82428',
+				data: [6,5,5,5,4,3,1],
+				barPercentage: 9,
+				barThickness: 20,
+				minBarLength: 100,
+			}];
+			let lfLabels = ["HTML,CSS,JS","C#","T-SQL","LINQ ","ASP.Net","PowerShell","Angular"]
+			let lfChartData =  Object.assign({}, chartConfig);
+			lfChartData.data = {
+				labels: lfLabels,
+				datasets: lfData
+			};
+
+			let tfData = [{
+				backgroundColor: '#c82428',
+				data: [5,5,4,4,1,1],
+				barPercentage: 9,
+				barThickness: 20,
+				minBarLength: 100,
+			}];
+			let tfLabels = ["Git","Microsoft Visual Studio","Azure DevOps / TFS","Jetbrains tooling","Jira","GitLab"]
+			let tfChartData =  Object.assign({}, chartConfig);
+			tfChartData.data = {
+				labels: tfLabels,
+				datasets: tfData
+			};
+
+			let mData = [{
+				backgroundColor: '#c82428',
+				data: [5,5,4,4,4],
+				barPercentage: 9,
+				barThickness: 20,
+				minBarLength: 100,
+			}];
+			let mLabels = ["Agile SCRUM","Object Oriented Programming","DevOps","UML", "Design Patterns"]
+			let mChartData =  Object.assign({}, chartConfig);
+			mChartData.data = {
+				labels: mLabels,
+				datasets: mData
+			};
+
+			new Chart(lfChart, lfChartData);
+			new Chart(tfChart, tfChartData);
+			new Chart(mChart, mChartData);
+
+
 		});
 	});
